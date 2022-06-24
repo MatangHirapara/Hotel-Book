05f2eb0482f2978f5413313849c782aa67a790d7
matang <matang.hirapara1224@gmail.com>
2022-06-24 17:00:06 +0530

    CarAxiox in new changes

--- src/component/pages/CarAxiox/Caraxiox.jsx
+++ src/component/pages/CarAxiox/Caraxiox.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Caraxiox = () => {
   return (
-    <div>Caraxiox</div>
+    <>
+      <div>
+        <ul>
+          <li>item 1</li>
+          <li>item 2</li>
+          <li>item 3</li>
+          <li>item 4</li>
+        </ul>
+        <ul>
+          <li>tea</li>
+          <li>coffee</li>
+        </ul>
+      </div>
+    </>
   )
 }
 
